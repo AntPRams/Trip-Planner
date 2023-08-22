bcef74a6afce5519853197eacfc294202fc67164
António Ramos <antonio.ramos.p@gmail.com>
2023-08-22 15:43:24 +0100

    Project files

---
+++ TuiCC/TuiCCApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuiCCApp.swift
+//  TuiCC
+//
+//  Created by António Ramos on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TuiCCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
