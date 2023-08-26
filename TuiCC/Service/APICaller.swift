aac403ad86575c05264177897d8a19f44b43ae6e
Antonio Ramos <antonio.ramos.p@gmail.com>
2023-08-26 15:12:43 +0100

    Moving files to where they belong

--- TuiCC/Service/APICaller.swift
+++ TuiCC/Service/APICaller.swift
@@ -13,3 +13,30 @@ final class APICaller: APICallerInterface {
         return fetchedData
     }
 }
+
+extension APICaller {
+    func mapResponse(response: (data: Data, response: URLResponse)) throws -> Data {
+        guard let httpResponse = response.response as? HTTPURLResponse else {
+            return response.data
+        }
+        
+        switch httpResponse.statusCode {
+        case 200..<300:
+            return response.data
+        case 300..<400:
+            throw NetworkError.redirected
+        case 400:
+            throw NetworkError.badRequest
+        case 401:
+            throw NetworkError.unauthorized
+        case 403:
+            throw NetworkError.forbidden
+        case 404:
+            throw NetworkError.notFound
+        case 500..<600:
+            throw NetworkError.serverError
+        default:
+            throw NetworkError.unknown
+        }
+    }
+}
